8a3fefcb1c79ac32949bce873a1bed3ba506bf5f
marrasluca <lmarras10@gmail.com>
2025-03-04 17:40:25 +0100

    add: endpoint

--- src/pages/Postslist.jsx
+++ src/pages/Postslist.jsx
@@ -3,6 +3,18 @@ import { useEffect, useState } from "react";
 
 
 const PostsList = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    const url = import.meta.env.VITE_ENDPOINT_URL
+
+    useEffect( () => {
+        axios.get( '')
+    })
+
+    return (
+        <h1>Lista Posts</h1>
+    )
     
 }
 

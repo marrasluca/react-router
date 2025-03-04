eac6bd9e43c2ac74efd55bc8661eced82772879a
marrasluca <lmarras10@gmail.com>
2025-03-04 20:03:44 +0100

    posts

--- src/pages/Postslist.jsx
+++ src/pages/Postslist.jsx
@@ -2,18 +2,50 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
+
 const PostsList = () => {
 
     const [posts, setPosts] = useState([]);
 
-    const url = import.meta.env.VITE_ENDPOINT_URL
+    const url = import.meta.env.VITE_ENDP0INT_URL
 
     useEffect( () => {
-        axios.get( '')
-    })
+        axios
+        .get(url)
+        .then ( (res) => setPosts(res.data))
+        .catch( (err) => console.error(err))
+    }, []);
 
     return (
-        <h1>Lista Posts</h1>
+        <>
+        <h1>Lista posts</h1>
+
+        <div className="container">
+            <div className="row">
+                {
+                    posts.map((elem) => {
+
+                        const { id, title, content, image, tags } = elem
+                        return (
+                            <div className="col-3">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h4 className="card-title">{title}</h4>
+                                        <p className="card-text">{tags}</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
+
+            </div>
+        </div> 
+        </>
+      
     )
     
 }
